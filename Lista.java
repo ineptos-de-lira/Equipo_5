ce9d230737b79b836e9cc83b06b32505f13e5533
Spectral-103 <up240712@alumnos.upa.edu.mx>
2025-11-29 15:18:44 -0600

    Mostrar pase de lista,agregado

--- Lista.java
+++ Lista.java
@@ -8,3 +8,10 @@ class Lista {
     estudiantes = new ArrayList<Estudiante>();
     sc = new Scanner(System.in);
   }
+
+    void mostrarPasedeLista() {
+    System.out.println("=============== Pase de Lista ===============");
+      for (Estudiante est : estudiantes) {
+        System.out.println(est.obtenerDatos());
+      }
+    }
