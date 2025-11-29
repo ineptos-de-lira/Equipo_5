326e0b12d7b01bfca849d0ba28376c0c45d67a36
Spectral-103 <up240712@alumnos.upa.edu.mx>
2025-11-29 15:29:51 -0600

    Implementacion de if

--- Lista.java
+++ Lista.java
@@ -11,7 +11,11 @@ class Lista {
 
     void mostrarPasedeLista() {
     System.out.println("=============== Pase de Lista ===============");
+    if (estudiantes.isEmpty()) {
+      System.out.println("No hay estudiantes registrados");
+    } else {
       for (Estudiante est : estudiantes) {
         System.out.println(est.obtenerDatos());
       }
     }
+  }
