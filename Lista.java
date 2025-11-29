84c5299ec94d14c7f0ca5a4b40c15bb6a34d79d3
Emilio <up240589@alumnos.upa.edu.mx>
2025-11-29 16:32:16 -0600

    Correcciones a Lista.Java

--- Lista.java
+++ Lista.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 class Lista {
-	private ArrayList<Estudiante> estudiantes;
+  private ArrayList<Estudiante> estudiantes;
   private Scanner sc;
 
   Lista() {
@@ -9,16 +10,17 @@ class Lista {
     sc = new Scanner(System.in);
   }
 
-void crearestudiante() {
-  System.out.println("Ingrese el nombre del estudiante");
-  String nombre = sc.nextLine();
-  System.out.println("Ingrese la matricula del estudiante");
-  String matricula = sc.nextLine();
-  String numero = String.valueOf(estudiantes.size() + 1);
-  Estudiante nuevoEstudiante = new Estudiante(numero, nombre);
-  estudiantes.add(nuevoEstudiante);
-  System.out.println("Estudiante registrado exitosamente");
-} 
+  void crearEstudiante() {
+    System.out.println("Ingrese el nombre del estudiante");
+    String nombre = sc.nextLine();
+    System.out.println("Ingrese la matricula del estudiante");
+    String matricula = sc.nextLine();
+    String numero = String.valueOf(estudiantes.size() + 1);
+    Estudiante nuevoEstudiante = new Estudiante(numero, nombre);
+    estudiantes.add(nuevoEstudiante);
+    System.out.println("Estudiante registrado exitosamente");
+  } 
+
   void mostrarPasedeLista() {
     System.out.println("=============== Pase de Lista ===============");
     if (estudiantes.isEmpty()) {
