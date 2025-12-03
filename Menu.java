494170eda524aff776256adb28051345d560faab
Axel0rtiz <113001587+Axel0rtiz@users.noreply.github.com>
2025-12-02 20:24:58 -0600

    Nombre de las funciones agregadas a cada opcion

--- Menu.java
+++ Menu.java
@@ -1,54 +1,57 @@
 import java.util.Scanner;
-clas Menu {
-    private Lista tareaManager;
-    private Scanner scanner;
-    private String opcion;
+
+class Menu {
+  private Lista tareaManager;
+  private Scanner scanner;
+  private String opcion;
     
-    Menu(){
-        tareaManager = new Lista();
-        scanner = new Scanner(System.in);
+  Menu() {
+    tareaManager = new Lista();
+    scanner = new Scanner(System.in);
+  }
+    
+  private void mostrarOpciones() {
+    System.out.println("\n --- Control de Asistencias ---");
+    System.out.println("1. Registrar nuevo estudiante: ");
+    System.out.println("2. Ver pase de lista: ");
+    System.out.println("3. MArcar asistencia de estudiantes: ");
+    System.out.println("4. Eliminar estudiante");
+    System.out.println("5. Salir del sistema: ");
+    System.out.println("Selecciona una opcion: ");
+  }
+    
+  private void procesarOpcion(String opcion) {
+    switch (opcion.trim()) {
+      case "1":
+        tareaManager.crearestudiante();
+        break;
+      case "2":
+        tareaManager.mostrarPasedeLista();
+        break;
+      case "3":
+        tareaManager.actualizarEstado();
+        break;
+      case "4":
+        tareaManager.eliminarestudiante();
+        break;
+      case "5":
+        System.out.println("Adios");
+        return;
+      default:
+        System.out.println("Eligue una opcion correcta");
+        break;
     }
+  }
     
-    private void mostrarOpciones(){
-        System.out.println("\n --- Control de Asistencias ---");
-        System.out.println("1. Registrar nuevo estudiante: ");
-        System.out.println("2. Ver pase de lista: ");
-        System.out.println("3. MArcar asistencia de estudiantes: ");
-        System.out.println("4. Eliminar estudiante");
-        System.out.println("5. Salir del sistema: ");
-        System.out.println("Selecciona una opcion: ");
+  void ejecutar() {
+    while (true) {
+      mostrarOpciones();
+      opcion = scanner.nextLine();
+      if (opcion.equals("5")) {
+        break;
+      }
+      procesarOpcion(opcion);
+      System.out.println(" ");
     }
-    
-    private void procesarOpcion(String opcion){
-        switch(opcion.trim()){
-            case"1":
-            break;
-            case"2":
-            break;
-            case"3":
-            break;
-            case"4":
-            break;
-            case"5":
-            System.out.println("Adios");
-            return;
-            default:
-            System.out.println("Eligue una opcion correcta");
-            break;
-        }
-    }
-    
-    void ejecutar(){
-        while(true){
-            mostrarOpciones();
-            opcion = scanner.nextLine();
-            if(opcion.equals("5")){
-                break;
-            }
-            procesarOpcion(opcion);
-            System.out.println(" ");
-        }
-    }
-    
-    
+  }  
 }
