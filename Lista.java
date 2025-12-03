bd6085f0c991b16a52139211f0d352cf830ce9e1
Emilio <up240589@alumnos.upa.edu.mx>
2025-12-02 19:39:41 -0600

    Creacion de atributos y metodo constructor

--- Lista.java
+++ Lista.java
@@ -1,4 +1,11 @@
-class Lista {
-	
-}
+import java.util.ArrayList;
+import java.util.Scanner;
 
+class Lista {
+  ArrayList<Estudiante> estudiantes;
+  Scanner sc;
+
+  Lista() {
+    estudiantes = new ArrayList<Estudiante>();
+    sc = new Scanner(System.in);
+  }
