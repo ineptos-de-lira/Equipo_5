61181ef8f2c92a21bd67160024c467e0f3483b4d
Spectral-103 <up240712@alumnos.upa.edu.mx>
2025-12-02 20:02:23 -0600

    Arreglao, inclusion de if

--- Lista.java
+++ Lista.java
@@ -2,18 +2,23 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class Lista {
-  private ArrayList<Estudiante> estudiantes;
-  private Scanner sc;
 
-  Lista() {
-    estudiantes = new ArrayList<Estudiante>();
-    sc = new Scanner(System.in);
-  }
+    private ArrayList<Estudiante> estudiantes;
+    private Scanner sc;
 
-  void mostrarPasedeLista() {
-    System.out.println("=============== Pase de Lista ===============");
-      for (Estudiante est : estudiantes) {
-        System.out.println(est.obtenerDatos());
-      }
-  }
+    Lista() {
+        estudiantes = new ArrayList<Estudiante>();
+        sc = new Scanner(System.in);
+    }
+
+    void mostrarPasedeLista() {
+        System.out.println("=============== Pase de Lista ===============");
+        if (estudiantes.isEmpty()) {
+            System.out.println("No hay estudiantes registrados");
+        } else {
+            for (Estudiante est : estudiantes) {
+                System.out.println(est.obtenerDatos());
+            }
+        }
+    }
 }
