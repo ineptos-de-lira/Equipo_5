737a85fefc17d17f0eff69ee3d05bd4302168d67
Emilio <up240589@alumnos.upa.edu.mx>
2025-12-02 19:55:24 -0600

    Creacion de Metodos para crear y borrar estudiantes

--- Lista.java
+++ Lista.java
@@ -9,4 +9,18 @@ class Lista {
     estudiantes = new ArrayList<Estudiante>();
     sc = new Scanner(System.in);
   }
-}
+  void crearestudiante() {
+    System.out.println("Digite el nombre del estudiante");
+    String nombre = sc.nextLine();
+    String numero = String.valueOf(estudiantes.size() + 1);
+    Estudiante nuevoEstudiante = new Estudiante(numero, nombre);
+    estudiantes.add(nuevoEstudiante);
+  }
+
+  void eliminarestudiante() {
+  System.out.println("Digite el numero del estudiante a eliminar");
+  int numero = sc.nextInt();
+  sc.nextLine();
+  numero--;
+  estudiantes.remove(numero);
+  }}
